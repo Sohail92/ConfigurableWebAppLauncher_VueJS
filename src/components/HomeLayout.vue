b77103408246007e1f009e6738e00afcfeafabd3
Sohail Rahman <contact@sohailrahman.net>
2019-08-06 23:48:32 +0100

    Updated view to contain 3 example applications to launch

--- src/components/HomeLayout.vue
+++ src/components/HomeLayout.vue
@@ -1,7 +1,7 @@
 <template>
  <v-app>
-  <v-navigation-drawer app>
-    <v-list>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list dense>
       <v-list-item v-for="item in items" :key="item.title" link>
         <v-list-item-icon>
           <v-icon>{{ item.icon }}</v-icon>
@@ -25,6 +25,31 @@
     <v-container fluid>
       <!-- If using vue-router -->
       <!--<router-view></router-view>-->
+
+      <v-card max-width="350" class="mx-auto">
+        <v-card-title>WeatherHub</v-card-title>
+        <v-card-text>Get weather information from a variety of providers</v-card-text>
+        <v-card-actions>
+          <v-btn text href="http://weatherhub.azurewebsites.net" target="_blank">Click</v-btn>
+        </v-card-actions>
+      </v-card>
+<br/>
+      <v-card max-width="350" class="mx-auto">
+        <v-card-title>To Do List</v-card-title>
+        <v-card-text>A list of your jobs to do</v-card-text>
+        <v-card-actions>
+          <v-btn text href="http://todolistme.net/" target="_blank">Click</v-btn>
+        </v-card-actions>
+      </v-card>
+<br>
+      <v-card max-width="350" class="mx-auto">
+        <v-card-title>Speed Test</v-card-title>
+        <v-card-text>Test your network connection speed</v-card-text>
+        <v-card-actions>
+        <v-btn text href="http://speedtest.net" target="_blank">Click</v-btn>
+      </v-card-actions>
+    </v-card>
+
     </v-container>
   </v-content>
 
