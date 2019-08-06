ab1d21753521ee1bcf8c597237c39e37eb4fb4d1
Sohail Rahman <contact@sohailrahman.net>
2019-08-06 19:00:15 +0100

    Removed template HelloWorld.vue and created AppDisplay.vue, changed pointers in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppDisplay msg="Welcome to Your Web App Launcher"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppDisplay from './components/AppDisplay.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppDisplay
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
