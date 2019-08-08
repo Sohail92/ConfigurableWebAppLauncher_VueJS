76ff1ef0d6137b09353104f8af7920250656de0b
Sohail Rahman <contact@sohailrahman.net>
2019-08-08 23:32:16 +0100

    Used google icons along with the item cards

--- src/components/HomeLayout.vue
+++ src/components/HomeLayout.vue
@@ -6,7 +6,10 @@
                     <v-list-item-title class="headline mb-1">Weather Hub</v-list-item-title>
                     <v-list-item-subtitle>Up to date weather info from a variety of providers, in one place!</v-list-item-subtitle>
                 </v-list-item-content>
-                <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+
+                <v-list-item-avatar tile size="80" color="white">
+                    <v-icon color="grey" x-large>wb_sunny</v-icon>
+                </v-list-item-avatar>
             </v-list-item>
             <v-card-actions>
                 <v-btn outlined color="blue" href="http://weatherhub.azurewebsites.net" target="_blank" text>Visit</v-btn>
@@ -18,7 +21,9 @@
                     <v-list-item-title class="headline mb-1">To Do List</v-list-item-title>
                     <v-list-item-subtitle>A handy app to use as a to-do list.</v-list-item-subtitle>
                 </v-list-item-content>
-                <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+                <v-list-item-avatar tile size="80" color="white">
+                    <v-icon color="grey" x-large>view_list</v-icon>
+                </v-list-item-avatar>
             </v-list-item>
             <v-card-actions>
                 <v-btn outlined color="blue" href="http://todolistme.net/" target="_blank" text>Visit</v-btn>
@@ -30,7 +35,9 @@
                     <v-list-item-title class="headline mb-1">Speed Test</v-list-item-title>
                     <v-list-item-subtitle>Test your Internet connection with this interactive speed test tool.</v-list-item-subtitle>
                 </v-list-item-content>
-                <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+                <v-list-item-avatar tile size="80" color="white">
+                    <v-icon color="grey" x-large>check_circle_outline</v-icon>
+                </v-list-item-avatar>
             </v-list-item>
             <v-card-actions>
                 <v-btn outlined color="blue" href="http://speedtest.net" target="_blank" text>Visit</v-btn>
